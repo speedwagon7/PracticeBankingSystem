0669a4d64c09572afa54b9548a5c33b648753847
hayden.poon <hayden.poon@fdmgroup.com>
2024-10-29 17:16:19 +0000

    added and fixed tests on current acc

--- src/test/java/org/example/banking/CurrentAccountTest.java
+++ src/test/java/org/example/banking/CurrentAccountTest.java
@@ -1,6 +1,7 @@
 package org.example.banking;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,17 +12,48 @@ class CurrentAccountTest {
     private CurrentAccount currentAccount;
 
     @BeforeEach
-    void setUp() {
-        currentAccount = new CurrentAccount();
+    public void setUp(){
+        currentAccount = new CurrentAccount(1000.0, 0, "Bob", 500);
     }
-
     @AfterEach
     void tearDown() {
         currentAccount = null;
     }
+    @Test
+    void testGetBalance() {
+        currentAccount.setBalance(1000.0);
+        Assertions.assertEquals(currentAccount.getBalance(), 1000.0);
+    }
+
+    @Test
+    void testGetMinimumBalance() {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.setMinimumBalance(0);
+        Assertions.assertEquals(bankAccount.getMinimumBalance(), 0);
+    }
+
+    @Test
+    void deposit() {
+        currentAccount.deposit(1000.0);
+        Assertions.assertEquals(currentAccount.getBalance(), 2000.0);
+    }
 
     @Test
     void testWithdraw() {
+        currentAccount.setBalance(1000.0);
+        currentAccount.withdraw(100.0);
+        Assertions.assertEquals(currentAccount.getBalance(), 900);
+    }
+
+    @Test
+    void testWithdrawOverMaximumWithdrawal() {
+        currentAccount.setBalance(1000.0);
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            currentAccount.withdraw(100000.0); // Trying to withdraw more than the max limit
+        });
+
+        // Verify the exception message
+        assertEquals("Cannot withdraw over maximum", exception.getMessage());
 
     }
 }
